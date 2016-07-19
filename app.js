3b940730e1816f9bd96e54161fb1db9084e8a5a7
Smyth <smytho@amazon.com>
2016-07-19 22:38:47 +0100

    adding geo location

--- app.js
+++ app.js
@@ -1,79 +1,142 @@
-$(document).ready(function() {
+var APP = {
 
-    var BASE_URL = 'https://www.crossorigin.me/http://api.irishrail.ie/realtime/realtime.asmx/';
-    var ALL_STATIONS = 'getAllStationsXML_WithStationType?StationType=D';
-    var TRAINS = 'getStationDataByCodeXML?StationCode=';
+    BASE_URL: 'https://www.crossorigin.me/http://api.irishrail.ie/realtime/realtime.asmx/',
+    STATIONS_URL: 'getAllStationsXML_WithStationType?StationType=D',
+    TRAINS_URL: 'getStationDataByCodeXML?StationCode=',
 
+    stations: [],
 
-    $('.station-list').change(function(event){
-        var station = event.target.value;
-        $.ajax({
-        url: BASE_URL + TRAINS + station,
-        beforeSend: function() {
-            $('.trains-list').empty().append("Loading...");
-        },
-        success : function(xml) {
-            $('.train-list').css('display', 'inherit');
+    init: function () {
+        var that = this;
+        $('.station-list').change(function (event) {
+            $('.train-list').css('display', 'none');
             $('.train-list table').empty();
-            
-            $('.train-list table').append('<thead><tr>'+
-            '<td>Due</td>'+
-            '<td>Origin</td>'+
-            '<td>Destination</td>'+
-            '<td>Status</td>'+
-            '<td>Lastlocation</td>'+
-            '<td>Late</td>'+
-            '<td>Exparrival</td>'+
-            '<td>Direction</td>'+
-            '</tr></thead>');
 
+            var station = event.target.value;
+            that.getTrainsForStation(station);
+        });
+        this.getAllStations();
+    },
 
-            $(xml).find('objStationData').each(function (index) {
-                $('.train-list table').append('<tr>'+
-                        '<td>'+$(this).find('Duein').text()+'</td>'+
-                        '<td>'+$(this).find('Origin').text()+'</td>'+
-                        '<td>'+$(this).find('Destination').text()+'</td>'+
-                        '<td>'+$(this).find('Status').text()+'</td>'+
-                        '<td>'+$(this).find('Lastlocation').text()+'</td>'+
-                        '<td>'+$(this).find('Late').text()+'</td>'+
-                        '<td>'+$(this).find('Exparrival').text()+'</td>'+
-                        '<td>'+$(this).find('Direction').text()+'</td>'+
+    getAllStations: function () {
+        var that = this;
+        $.ajax({
+            url: this.BASE_URL + this.STATIONS_URL,
+            beforeSend: function () {
+                $('.station-list').empty().append(that.getStationOptionHTML("loading", "Loading..."));
+            },
+            success: function (xml) {
+                var stations = $(xml).find('objStation');
+                $(stations).each(function (index, stationXML) {
+                    var station = {
+                        code: $(stationXML).find('StationCode').text(),
+                        name: $(stationXML).find('StationDesc').text(),
+                        latitude: Number($(stationXML).find('StationLatitude').text()),
+                        longitude: Number($(stationXML).find('StationLongitude').text())
+                    };
+                    that.stations.push(station);
+                });
+                that.initGeo();
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    },
+
+    updateStationList: function () {
+        var $stationList = $('.station-list').empty();
+        this.stations.forEach(function (station) {
+            $stationList.append(this.getStationOptionHTML(station.code, station.name));
+        }, this);
+    },
+
+    getTrainsForStation: function (station) {
+        var that = this;
+        $.ajax({
+            url: that.BASE_URL + that.TRAINS_URL + station,
+            beforeSend: function () {
+                $('.trains-list').empty().append("Loading...");
+            },
+            success: function (xml) {
+                $('.train-list').css('display', 'inherit');
+                $('.train-list table').empty();
+
+                $('.train-list table').append('<thead><tr>' +
+                    '<td>Due</td>' +
+//                 '<td>Origin</td>'+
+                    '<td>Destination</td>' +
+//                 '<td>Status</td>'+
+//                 '<td>Lastlocation</td>'+
+                    '<td>Late</td>' +
+                    '<td>Exparrival</td>' +
+                    '<td>Direction</td>' +
+                    '</tr></thead>');
+
+                $(xml).find('objStationData').each(function () {
+                    $('.train-list table').append('<tr>' +
+                        '<td>' + $(this).find('Duein').text() + '</td>' +
+//                             '<td>'+$(this).find('Origin').text()+'</td>'+
+                        '<td>' + $(this).find('Destination').text() + '</td>' +
+//                             '<td>'+$(this).find('Status').text()+'</td>'+
+//                             '<td>'+$(this).find('Lastlocation').text()+'</td>'+
+                        '<td>' + $(this).find('Late').text() + '</td>' +
+                        '<td>' + $(this).find('Exparrival').text() + '</td>' +
+                        '<td>' + $(this).find('Direction').text() + '</td>' +
                         '</tr>');
                 });
-        },
-        error: function(error){
-            console.log(error);
-        }
-    });
-    })
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    },
 
-    var getTrainRowHTML = function(value, name) {
-        return '<option value="'+value+'">'+name+'</option>';
-    };
+    initGeo: function () {
+        var that = this;
+        navigator.geolocation.getCurrentPosition(
+            function (position) {
+                var latitude = position.coords.latitude;
+                var longitude = position.coords.longitude;
 
-    var getStationOptionHTML = function(value, name) {
-        return '<option value="'+value+'">'+name+'</option>';
-    };
+                that.updateStationList();
 
-    $.ajax({
-        url: BASE_URL + ALL_STATIONS,
-        beforeSend: function() {
-            $('.station-list').empty().append(getStationOptionHTML("loading", "Loading..."));
-        },
-        success : function(xml) {
-            var stations = $(xml).find('objStation');
-            var $stationList = $('.station-list').empty();
-            
-            $stationList.append(getStationOptionHTML('', "Select Station"));
+                var station = that.selectNearestStation(latitude, longitude);
+                var $stationList = $('.station-list');
 
-            $(stations).each(function(index, station) {
-                var stationCode = $(station).find('StationCode').text();
-                var stationName = $(station).find('StationDesc').text();
-                $stationList.append(getStationOptionHTML(stationCode, stationName));
-            })
-        },
-        error: function(error){
-            console.log(error);
-        }
-    });
+                $stationList.val(station.code);
+                $stationList.trigger('change');
+            },
+            function error(error) {
+                console.log("Unable to retrieve your location: " + error);
+            });
+    },
+
+    selectNearestStation: function (latitude, longitude) {
+        var nearestDistance = null;
+        var nearestStation = null;
+        this.stations.forEach(function (station) {
+            var distance = this.getDistance(latitude, longitude,
+                station.latitude, station.longitude);
+            if (!nearestDistance || distance < nearestDistance) {
+                nearestDistance = distance;
+                nearestStation = station;
+            }
+        }, this);
+        return nearestStation;
+    },
+
+    getDistance: function (x1, y1, x2, y2) {
+        var xDiff = x1 - x2;
+        var yDiff = y1 - y2;
+        return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+    },
+
+    getStationOptionHTML: function (value, name) {
+        return '<option value="' + value + '">' + name + '</option>';
+    }
+};
+
+$(document).ready(function () {
+    APP.init();
 });
